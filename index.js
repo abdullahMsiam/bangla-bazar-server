9e54304a23c76577cbbef7c278f03063ae660e0b
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2024-06-05 16:15:31 +0600

    server added

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = 3000
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gtc9ihn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+        await client.connect();
+
+        const userDB = client.db("userDB");
+        const userCollection = userDB.collection("userCollection");
+
+        app.post("/user", async (req, res) => {
+            const user = {
+                name: "siam",
+                age: 45,
+                mobile: 1221233434
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+            console.log(result);
+        })
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
